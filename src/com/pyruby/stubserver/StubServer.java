dae98717047625fde9df9486fceb66319be6ba99
Equal Experts <ee@foxwoods.(none)>
2013-05-07 12:57:11 +0100

    Added getLocalPort method.

--- src/com/pyruby/stubserver/StubServer.java
+++ src/com/pyruby/stubserver/StubServer.java
@@ -103,6 +103,14 @@ public class StubServer {
         }
     }
 
+    /**
+     * Returns the assigned port number - useful for when the server was created with port number of "0" so that the actual
+     * ephemeral port number can be retrieved.
+     */
+    public int getLocalPort() {
+        return server.getConnectors()[0].getLocalPort();
+    }
+
     private class StubHandler extends AbstractHandler {
         public void handle(String target, HttpServletRequest httpServletRequest, HttpServletResponse httpServletResponse, int dispatch) throws IOException, ServletException {
             for (Expectation expected : expectations) {
